ef8ef4fa6a80d684a525d3f3ccb6c5f66c396446
Eddie Santana <86499043+EddieSantana21@users.noreply.github.com>
2024-09-20 08:52:15 -0300

    tela criar conta

---
+++ bibliowl/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BibliOwl</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <section>
+        <div class="page">
+            <form method="POST" class="formLogin" action="components/validacao/validarLogin.php">
+                <h1>Login</h1>
+                <p>Digite os seus dados de acesso no campo abaixo.</p>
+                <label for="email">E-mail</label>
+                <input type="email" placeholder="Digite seu e-mail" autofocus="true" />
+                <label for="password">Senha</label>
+                <input type="password" placeholder="Digite sua senha" />
+
+                <a href="/">Esqueci minha senha</a>
+                
+                <input type="submit" value="Acessar" class="btn" />
+                <!-- <input type="button" value="Criar conta" class="btn" /> -->
+                <a href="components/page.php" class="text-criar-conta">Criar conta</a>
+            </form>
+        </div>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
