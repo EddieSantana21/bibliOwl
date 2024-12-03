75115288812c5a5c9a59bdbc5f4344dec43e3463
Wellington A.S. Aguiar <103387718+WellingtonAguiar@users.noreply.github.com>
2024-12-02 22:14:47 -0300

    alteração form login

--- bibliowl/pages/login.php
+++ bibliowl/pages/login.php
@@ -1,3 +1,7 @@
+<?php
+    include('../Model/Conexao2.php');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -12,21 +16,21 @@
 <body>
     <section>
         <div class="page">
-            <!-- <form method="POST" class="formLogin" action="components/validacao/validarLogin.php"> -->
-            <form method="POST" class="formLogin" action="../components/validacao/validarLogin.php">
+            <form action="../components/validacao/validarLogin.php" method="POST" id="form" class="formLogin">
                 <h1>Login</h1>
                 <p>Digite os seus dados de acesso no campo abaixo.</p>
                 <label for="email">E-mail</label>
-                <input type="email" id="emailUsuario" name="emailUsuario" placeholder="Digite seu e-mail" autofocus="true" />
+                <input type="email" name="email" id="email" placeholder="Digite seu e-mail" autofocus="true" />
                 <label for="password">Senha</label>
-                <input type="password" id="senhaUsuario" name="senhaUsuario" placeholder="Digite sua senha" />
+                <input type="password" name="senha" id="senha" placeholder="Digite sua senha"/>
 
                 <a href="/">Esqueci minha senha</a>
-
-                <input type="submit" value="Acessar" class="btn" />
+                
+                <input type="submit" value="Acessar" class="btn" id="submit"/>
+                <!-- <input type="button" value="Criar conta" class="btn" /> -->
                 <a href="register/cadastro1.php" class="text-criar-conta">Criar conta</a>
-            </form>
 
+            </form>
         </div>
     </section>
 
