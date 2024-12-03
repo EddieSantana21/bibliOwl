27e832e1cbbb6745ed4b6357a290db046988caaf
Wellington A.S. Aguiar <103387718+WellingtonAguiar@users.noreply.github.com>
2024-12-02 22:10:37 -0300

    add: validar login funcionando

--- bibliowl/components/validacao/validarLogin.php
+++ bibliowl/components/validacao/validarLogin.php
@@ -1,28 +1,21 @@
 <?php
-require_once("../../Model/Conexao.php");
-require_once("../../Model/Usuario.php");
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    
+        include_once("../../Model/Conexao2.php");
 
-if (isset($_POST['emailUsuario']) && isset($_POST['senhaUsuario'])) {
-    $emailUsuario = $_POST['emailUsuario'];
-    $senhaUsuario = $_POST['senhaUsuario'];
 
-$usuario = new Usuario();
-$usuario->setEmailUsuario($emailUsuario);
-$usuario->setSenhaUsuario($senhaUsuario);
-echo "deu certo até aqui";
-foreach ($usuario as $rowUsuario) {
-    echo $rowUsuario->listar($usuario);
-}
-$row = $usuario->listar($usuario);
-    if ($emailUsuario === @$row[0]['email_usuario'] && $senhaUsuario === @$row[0]['senha_usuario']) {
-        session_start();
-        $_SESSION['email_usuario'] = $emailUsuario;
-        $_SESSION['senha_usuario'] = $senhaUsuario;
-        header("location: ../../pages/home.php");
-    } else {
-        header("location: ../../pages/login.php");
-    } 
+        $sql = "SELECT * FROM tbUsuarios WHERE email_usuario = '$email' and senha_usuario = '$senha'";
+        $result = $conexao->query($sql);
+       
+        if(mysqli_num_rows($result) < 1)
+        {
+            header("location: ../../pages/login.php");
+        } else {
+            header("location: ../../home.php");
+        }
 
-}
+
+    
 
 ?>
